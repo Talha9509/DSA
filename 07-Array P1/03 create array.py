178adbc1083fdb3ee485bf6e5154afcc54b12fc3
Mohd Talha <mawtalha5112004@gmail.com>
2026-06-28 16:50:02 +0530

    7 - Array P1

---
+++ 07-Array P1/03 create array.py
@@ -0,0 +1,48 @@
+# creation
+from array import *
+
+arr1=array('i',[1,2,3])
+
+# insertion
+# arr1.insert(6,9)
+# print(arr1)
+# arr1.insert(4,6)
+# print(arr1)
+# arr1.insert(1,0)
+# print(arr1)
+
+# traversing
+# def trav(arr):
+#     for i in arr:
+#         print(i)
+
+# trav(arr1)
+
+# accessing
+# arr1[0]
+# print(arr1[0])
+
+# finding an element 
+# arr2=array('i',[9,2,4,5,1])
+
+# def findElem(arr,a):
+#     # for i in range(0,len(arr)):
+#     # for idx, val in enumerate(arr):
+#     #     if arr[i]==a:
+#     #         print(f"Element found at {i}")
+#     #         break
+#     # else:
+#     #     print("not found")
+
+#     for i in arr:
+#         if i==a:
+#             return arr.index(a)
+#     return "not found"
+
+
+# print(findElem(arr2,9))
+# print(findElem(arr2,4))
+
+# deleting
+# arr1.remove(1)
+# print(arr1)
---
+++ 07-Array P1/11 all operations.py
@@ -0,0 +1,58 @@
+
+from array import *
+
+arr1=array('d',[4,6,3,92,4])
+
+# for i in arr1:
+#     print(i)
+
+# arr1.append(3)
+# print(arr1)
+
+# arr1.insert(1,5)
+# print(arr1)
+
+# arr2=array('d',[1,2,3])
+# arr1.extend(arr2)
+# print(arr1)
+
+# add items from list into array using fromlist()
+# list=[1,5,3]
+# arr1.fromlist(list)
+# print(arr1)
+
+# print(arr1.pop())
+# print(arr1)
+
+# index usinf index()
+# print(arr1.index(3))
+
+# arr1.reverse()
+# print(arr1)
+
+#11. Get array buffer information through buffer_info() method
+# print(arr1.buffer_info())
+
+#12. Check for number of occurrences of an element using count() method
+# print(arr1.count(4))
+
+#13. Convert array to string using tostring() method
+# a=arr1.tobytes()
+# print(a)
+# ints=array('d')
+# ints.frombytes(a)
+# print(ints)
+
+#14. Convert array to a python list with same elements using tolist() method
+# print(arr1.tolist())
+
+#15. Append a string to char array using fromstring() method
+# chars = array('b', b'a')
+# print(chars)
+# str_to_append=" bc"
+# chars.frombytes(str_to_append.encode('utf-8'))
+# print(chars)
+# print(chars.tobytes().decode('utf-8'))
+
+#16. Slice Elements from an array
+# print(arr1[:2])
